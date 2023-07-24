4aa070ac4cb28de3d0516531884753b18faec1b9
Kevin Kellar <kellar.kevin@gmail.com>
2023-07-23 19:42:47 -0700

    aya

--- setup/gui.sh
+++ setup/gui.sh
@@ -8,6 +8,9 @@ sudo apt update
 sudo apt upgrade
 sudo apt install gnome-tweaks
 
+gsettings set org.gnome.desktop.input-sources xkb-options \[\'caps:ctrl_modifier\'\] 
+gsettings set org.gnome.desktop.interface enable-animations true
+
 # Cannot merge into single line
 sudo snap install code --classic
 sudo snap install chromium
